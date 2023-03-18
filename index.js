6adb52d44ba507c907d1d1851591ebbcc1358622
NEWUSER <chasevriend84@gmail.com>
2023-03-18 10:28:07 -0700

    1st commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+ReactDOM.render(
+    <div>
+        <h1>Let's Learn React</h1>
+        <p>My Favorite Foods</p>
+        <ul>
+            <li>Sushi</li>
+            <li>Cheeseburger</li>
+            <li>Cookies</li>
+        </ul>
+    </div>, 
+    document.getElementById("root")
+);
